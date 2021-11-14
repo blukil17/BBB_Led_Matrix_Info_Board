a450f178719c189591ba570600ec797ffedd4be0
Andrew Weger <wegerac@rose-hulman.edu>
2021-11-14 02:48:16 -0500

    update testing Comms

--- testingCommunication.py
+++ testingCommunication.py
@@ -11,24 +11,31 @@ univ = 64
 chan = 32
 row = []
 rows = [row]*univ
-for i in range(0,chan):
-        row.append(128)
-        row.append(0)
-        row.append(0)
+# for i in range(0,chan):
+#         row.append(128)
+#         row.append(0)
+#         row.append(0)
 
-for u in range(1,univ+1):
-        sender.activate_output(u)  # start sending out data in the 1st universe
-        sender[u].destination = "192.168.7.2"  # or provide unicast information
-#       #row=[]
-#       for i in range(0,chan):
-#               #if(i%3==0):
-#               row.append(0)
-#               row.append(0)
-#               row.append(100)
-#sender[1].dmx_data = row
-        print(row)
-        print(len(row))
-        rows[u-1] = row
-        sender[u].dmx_data = rows[u-1]  # some test DMX data
-        time.sleep(.01)
+for j in range(0,255):
+        row = []
+        for i in range(0,chan):
+                row.append(0)
+                row.append(0)
+                row.append(j)
+        print(j)
+        for u in range(1,univ+1):
+                sender.activate_output(u)  # start sending out data in the 1st universe
+                sender[u].destination = "192.168.7.2"  # or provide unicast information
+        #       #row=[]
+        #       for i in range(0,chan):
+        #               #if(i%3==0):
+        #               row.append(0)
+        #               row.append(0)
+        #               row.append(100)
+        #sender[1].dmx_data = row
+                #print(row)
+                #print(len(row))
+                rows[u-1] = row
+                sender[u].dmx_data = rows[u-1]  # some test DMX data
+                time.sleep(.001)
 sender.stop()  # do not forget to stop the sender
